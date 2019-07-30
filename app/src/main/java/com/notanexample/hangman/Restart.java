f83fcd91135bf5de3fbd31dc4af603a72da4e1aa
teddyyuan <41097847+teddyyuan@users.noreply.github.com>
2019-07-29 22:52:46 -0400

    Add files via upload
    Adding restart activity

---
+++ app/src/main/java/com/notanexample/hangman/Restart.java
@@ -0,0 +1,9 @@
+package com.notanexample.hangman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Restart {
+    public static void restartApp(AppCompatActivity appCompatActivity){
+        appCompatActivity.recreate();
+    }
+}
